ca8a7872a9cf0ff6ad39ca0d115a657e51c5ebea
heba <hebamotawei@gmail.com>
2019-05-17 15:04:49 +0300

    Required modifications

--- database_setup.py
+++ database_setup.py
@@ -2,7 +2,7 @@ from datetime import datetime as dt
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -22,8 +22,12 @@ class Cuisine(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, backref=backref(
+        "cuisines", cascade="all,delete"))
+
+    # recipes = relationship('Recipe', backref='cuisine', cascade="all,delete")
 
     @property
     def serialize(self):
@@ -41,10 +45,14 @@ class Recipe(Base):
     name = Column(String(250), nullable=False)
     description = Column(String(250))
     creationDate = Column(DateTime, index=True, default=dt.utcnow)
+
     cuisine_id = Column(Integer, ForeignKey('cuisine.id'))
-    cuisine = relationship(Cuisine, backref='recipes')
+    cuisine = relationship(Cuisine, backref=backref(
+        "recipes", cascade="all,delete"))
+
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship(User, backref=backref(
+        "recipes", cascade="all,delete"))
 
     @property
     def serialize(self):
